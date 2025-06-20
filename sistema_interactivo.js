4f7e3be2cb16b44c3fddc2d9fa67f67a7536c31f
marianarestrepo26 <marianartpo08@gmail.com>
2025-06-19 20:12:54 -0500

    Sistema Interactivo

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,31 +1,36 @@
-function isNumberInt(age) {
-  return !isNaN(age) && Number.isInteger(parseFloat(age));
+function isString(name) {
+  let isAlphabetic = /^[A-Za-z\s]+$/.test(name);
+  while (isAlphabetic === false){
+    alert("Error, enter a valid name.");
+    console.error("Error, enter a valid name");
+    name = prompt("What is your name?");
+    isAlphabetic = /^[A-Za-z]+$/.test(name);
+  }
+  return name;  
 }
 
-function isString(name) {
-    
+function isNumberInteger(number) {
+  while (isNaN(number) || number < 0){
+    alert("Error, enter a valid age.");
+    console.error("Error, enter a valid age");
+    number = prompt("How old are you?");
+  }
+  return number;  
 }
 
 let userName = prompt("What is your name?");
+let nameValidate = isString(userName)
 
-if (!userName || userName.trim() === "") {
-  alert("Error, enter a valid name");
-} 
-else {
-  let userAge = prompt("How old are you?");
-    if (isNumberInt(userAge)) {
-    userAge = parseInt(userAge);
-    if (userAge < 18) {
-      alert(`Hello ${userName}, you're a little young at the moment, but don't get discouraged. You can join the team soon.`);
-      console.error(`Sorry, we'll see you next time ${userName}.`)
-    } 
-    else {
-      alert(`Hola ${userName}, you are of legal age. welcome to this learning space.`);
-      console.log(`Welcome to my crib ${userName}`)
-    }
+let userAge = prompt("How old are you?");
+if (isNumberInteger(userAge)) {
+  userAge = parseInt(userAge);
+  if (userAge < 18) {
+    alert(`Hello ${nameValidate}, you're a little young at the moment, but don't get discouraged. You can join the team soon.`);
+    console.error(`Sorry, we'll see you next time ${nameValidate}.`)
   } 
   else {
-    console.error("Error: Por favor, ingresa una edad válida en números.");
-    alert("La edad ingresada no es válida. Inténtalo nuevamente.");
+    alert(`Hello ${nameValidate}, you are of legal age. welcome to this learning space.`);
+    console.log(`Welcome to my crib ${nameValidate}`)
   }
-}
+} 
+
